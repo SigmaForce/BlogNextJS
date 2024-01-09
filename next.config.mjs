3755249d2b9a6f6345f0a08d816b75d82782141c
Leonardo Matos <leonardolucas2020@gmail.com>
2024-01-08 22:30:38 -0400

    :chore: add vercel plugin and decap cms config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,29 @@
 // next.config.js
 import { withContentlayer } from 'next-contentlayer';
+import withPlugins from 'next-compose-plugins';
+
+import withPWAInit from 'next-pwa';
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  exclude: [
+    ({ asset }) => {
+      if (
+        asset.name.startsWith('server/') ||
+        asset.name.match(
+          /^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/
+        )
+      ) {
+        return true;
+      }
+
+      return isDev && !asset.name.startsWith('static/runtime/');
+    }
+  ]
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -8,4 +32,4 @@ const nextConfig = {
   }
 };
 
-export default withContentlayer({ nextConfig });
+export default withPlugins([withPWA, withContentlayer], nextConfig);
